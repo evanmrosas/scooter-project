d2252492f5aad01c15addcc9522b638b4f1118d0
evanmrosas <evanmrosas@gmail.com>
2024-09-13 15:03:16 -0500

    working on registerUser method

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -13,6 +13,25 @@ class ScooterApp {
     }
     this.registeredUsers = {}
   }
+
+  registerUser(username, password, age){
+    if(this.registeredUsers[username]){
+      throw new Error('already registered')
+    }
+    else if(age < 18){
+      throw new Error('too young to register')
+    }
+    else{
+      let user = new User(username, password, age)
+      this.registeredUsers["User"] = user
+      console.log(this.registeredUsers)
+    }
+  }
 }
+
+const lime = new ScooterApp()
+
+lime.registerUser('evanrosas', '1234', 24)
+lime.registerUser('evanrosas', '1234', 24)
 
 module.exports = ScooterApp
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+const newUser = new User(username, password, age)
